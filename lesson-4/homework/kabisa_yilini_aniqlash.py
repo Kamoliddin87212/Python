ed705c8715fe3030e247f189fee1fa9ea3b2e851
Kamoliddin87212 <k.abdugafarov@gmail.com>
2025-08-14 18:09:27 +0500

    Update kabisa_yilini_aniqlash.py

--- lesson-4/homework/kabisa_yilini_aniqlash.py
+++ lesson-4/homework/kabisa_yilini_aniqlash.py
@@ -1,1 +1,6 @@
-
+# Kabisa yilini aniqlash
+year = int(input("Yilni kiriting: "))
+if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
+    print(f"{year} yil kabisa yili.")
+else:
+    print(f"{year} yil kabisa yili EMAS.")
