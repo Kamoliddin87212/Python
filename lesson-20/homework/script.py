222b568929cc804b69249ed19ae678a2af1304ec
Kamoliddin87212 <k.abdugafarov@gmail.com>
2025-09-11 18:16:11 +0500

    Update script.py

--- lesson-20/homework/script.py
+++ lesson-20/homework/script.py
@@ -1,1 +1,16 @@
+## Exercise 1: Customer Purchases Analysis
+import sqlite3
+import pandas as pd
+conn = sqlite3.connect('chinook.db')
+df = pd.read_sql("""
+SELECT c.CustomerId, c.FirstName || ' ' || c.LastName as Name, SUM(i.Total) as Total
+FROM customers c
+JOIN invoices i ON c.CustomerId = i.CustomerId
+GROUP BY c.CustomerId
+ORDER BY Total DESC
+LIMIT 5
+""", conn)
+print(df)
 
+## Exercise 2: Album vs. Individual Track Purchases
+df = pd.read_sql("SELECT * FROM invoice_items", conn)
