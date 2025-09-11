5679076cd30c60259233c9c5d315637aa1cd97cf
Kamoliddin87212 <k.abdugafarov@gmail.com>
2025-09-11 18:03:49 +0500

    Update script.py

--- lesson-15/homework/script.py
+++ lesson-15/homework/script.py
@@ -1,1 +1,12 @@
-
+## Exercises 1-4:
+import sqlite3
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE Roster (Name TEXT, Species TEXT, Age INT)''')
+values = [('Benjamin Sisko', 'Human', 40), ('Jadzia Dax', 'Trill', 300), ('Kira Nerys', 'Bajoran', 29)]
+c.executemany('INSERT INTO Roster VALUES (?,?,?)', values)
+c.execute("UPDATE Roster SET Name = 'Ezri Dax' WHERE Name = 'Jadzia Dax'")
+c.execute("SELECT Name, Age FROM Roster WHERE Species = 'Bajoran'")
+print(c.fetchall())
+conn.commit()
+conn.close()
